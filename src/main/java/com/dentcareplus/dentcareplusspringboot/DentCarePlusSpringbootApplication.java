449cfd59bc93e7ca56993e6f1a09dc2b48f42e61
Minindu Pasan <minindupasan@gmail.com>
2024-10-10 23:20:47 +0530

    initial

---
+++ src/main/java/com/dentcareplus/dentcareplusspringboot/DentCarePlusSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dentcareplus.dentcareplusspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentCarePlusSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentCarePlusSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dentcareplus/dentcareplusspringboot/DentCarePlusSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentcareplus.dentcareplusspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentCarePlusSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
