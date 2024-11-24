04fd028f40f2933df3c1d4f4a17028613a9068fe
Nadil Dulran <draupadimoksha@gmail.com>
2024-11-25 00:09:52 +0530

    Added Inventory DTO

---
+++ src/main/java/com/dentcareplus/dentcareplusspringboot/dto/InventoryDTO.java
@@ -0,0 +1,21 @@
+package com.dentcareplus.dentcareplusspringboot.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class InventoryDTO {
+
+    private Long inventoryId;
+    private String itemName;
+    private int quantity;
+    private String statusLevel;
+    private int restockLevel;
+    private LocalDate purchaseDate;
+    private LocalDate expiryDate;
+    private BigDecimal unitCost;
+    private BigDecimal totalCost;
+    private LocalDate lastUpdated;
+}
