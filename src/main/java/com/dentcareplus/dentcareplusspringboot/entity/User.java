9cfe6004712205709d08e85096e0ac5237a75103
Nadil Dulran <draupadimoksha@gmail.com>
2024-11-25 11:57:52 +0530

    Added User Class and Enumerated Roles

---
+++ src/main/java/com/dentcareplus/dentcareplusspringboot/entity/User.java
@@ -0,0 +1,30 @@
+package com.dentcareplus.dentcareplusspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/dentcareplus/dentcareplusspringboot/enums/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.dentcareplus.dentcareplusspringboot.enums;
+
+public enum RoleEnum {
+    DENTIST,
+    ASSISTANT,
+    RECEPTIONIST
+}
